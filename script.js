954bdfede6ccf17b66b30e58b327b42d51a8de60
CarlosMensales <carlos.mensales@gmail.com>
2024-07-15 17:48:55 -0300

    Versão desktop finalizada

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function criptografar(msg) {
+function criptografar() {
+    let msg = document.getElementById('texto_f').value
     let outputText = msg
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
@@ -6,10 +7,11 @@ function criptografar(msg) {
         .replace(/o/g, "ober")
         .replace(/u/g, "uifat");
 
-    console.log(outputText)
+    exibirMensagem(outputText)
 }
 
-function descriptografar(msg) {
+function descriptografar() {
+    let msg = document.getElementById('texto_f').value
     let outputText = msg
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
@@ -17,5 +19,33 @@ function descriptografar(msg) {
         .replace(/ober/g, "o")
         .replace(/uifat/g, "u");
 
-    console.log(outputText)
+    exibirMensagem(outputText)
+}
+
+function exibirMensagem(msg) {
+    img = document.getElementById('img2')
+    p = document.getElementById('p2')
+    if(p && img) {
+        img.remove()
+        p.remove()    
+    }
+    h2 = document.getElementById('output')
+    h2.textContent = msg
+    let botao_copiar = document.getElementById('botao_copiar')
+    botao_copiar.classList.add('botao_copiar')
+    botao_copiar.style.display = 'inline'
+    botao_copiar.addEventListener('click', copiarMensagem);
+    
+}
+
+function copiarMensagem() {
+    const h2 = document.getElementById('output');
+    const mensagem = h2.textContent;
+    navigator.clipboard.writeText(mensagem)
+        .then(() => {
+            alert('Mensagem copiada para a área de transferência');
+        })
+        .catch(err => {
+            console.log('Erro ao copiar a mensagem: ' + err);
+        });
 }
